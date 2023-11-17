f2014543343bb02009a2efdeb601a0c276f3da0e
Aifert Yet <aifertyet0628@gmail.com>
2023-11-17 15:37:55 +0800

    basic structure and functionality done

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
+var prompt_arr = [];
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+})
+
+app.post("/submit", (req,res) => {
+    prompt_arr.push(req.body["prompt"]);
+    res.render("index.ejs", {
+        listOfItems : prompt_arr
+    })
 })
 
 app.listen(port, () => {
